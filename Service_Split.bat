a59e9602435377c83f65b80bf8f0cd93605902ba
Sean <61057625+ShadowWhisperer@users.noreply.github.com>
2024-12-09 16:54:22 -0600

    Updated
    Windows 11 no longer supports wmic. Changed to PowerShell.

--- Service_Split.bat
+++ Service_Split.bat
@@ -1,28 +1,28 @@
 @echo off
 
 ::
-:: Reduce the number of svchost.exe processes that appear in the Taskmanager
-::
-:: Run the script and reboot.
-::
-:: If you add or remove RAM, run this script again, and reboot
-::
-::
 :: Creator: ShadowWhisperer
 ::  Github: https://github.com/ShadowWhisperer
-:: Created: ?/?/2019
-:: Updated: 10/12/2023
+:: Created: 2019
+:: Updated: 12/09/2024
+::
+:: Reduce the number of svchost.exe processes that appear in the Taskmanager
+:: Run the script and reboot.
+:: If you add or remove RAM, run this script again, and reboot
+::
 ::
 
 :: Check if ran as Admin
 net session >nul 2>&1 || (echo. & echo Run Script As Admin & echo. & pause & exit)
 
-:: Get RAM in KB
-for /f "skip=1" %%p in ('wmic os get TotalVisibleMemorySize') do (
-set m=%%p
-goto :done)
+:: Get RAM in KB using PowerShell
+for /f "tokens=*" %%p in ('powershell -NoProfile -Command "& {(Get-CimInstance -ClassName Win32_OperatingSystem).TotalVisibleMemorySize}"') do (
+    set m=%%p
+    goto :done
+)
 :done
 set "HEX=%m%"
+
 :: Convert HEX to Dec
 set /A DEC=0x%HEX%
 
